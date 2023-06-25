2d3ef745725169618567475b33d7f3b8e8187d56
Tiago Tavares Lopes <tiagoltavares2002@gmail.com>
2023-06-25 17:05:09 -0300

    adicionando configuração para gravar a execução dos testes

--- playwright.config.ts
+++ playwright.config.ts
@@ -27,7 +27,8 @@ export default defineConfig({
     baseURL: 'https://loginxp.vercel.app',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry'
+    trace: 'on-first-retry',
+    video: 'on'
   },
 
   /* Configure projects for major browsers */
